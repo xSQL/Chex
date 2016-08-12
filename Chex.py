ae0485caf85be2e53aca058e6e758c6c0774c1e8
Sergey Logoshko <logoshko.s@gmail.com>
2016-08-12 10:20:28 +0000

    changes

--- Chex.py
+++ Chex.py
@@ -2,18 +2,6 @@ import random
 import math
 
 
-
-import time
-    
-class Profiler(object):
-    def __enter__(self):
-        self._startTime = time.time()
-            
-    def __exit__(self, type, value, traceback):
-        print("Elapsed time: {:.6f} sec".format(time.time() - self._startTime))
-
-
-
 class OverRangeException(Exception):
     """..."""
     
